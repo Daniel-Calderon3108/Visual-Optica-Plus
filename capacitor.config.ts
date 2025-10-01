825f600cdd0ad35ba1c58ae23bf8813cb6901c9e
Daniel Calderon <danielcalderon105@gmail.com>
2025-09-30 21:29:39 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'visual-optica-plus',
+  webDir: 'www'
+};
+
+export default config;
